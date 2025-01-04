41b34482d2f15f954cbd2116dd72173b02b818e7
Batug Kocak <batugkocak@gmail.com>
2025-01-04 18:40:33 +0300

    feat: add Post and User entity structs and update repository methods for creation

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,11 +3,25 @@ package store
 import (
 	"context"
 	"database/sql"
+	"time"
+
+	"github.com/lib/pq"
 )
+
+// Entity
+type Post struct {
+	ID        int64     `json:"id"`
+	Content   string    `json:"content"`
+	Title     string    `json:"title"`
+	UserId    int64     `json:"user_id"`
+	Tags      []string  `json:"tags"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
 
 // PostRepository
 type PostRepository interface {
-	Create(context.Context) error
+	Create(context.Context, *Post) error
 }
 
 // PostRepository Implementation
@@ -15,7 +29,19 @@ type PostStore struct {
 	db *sql.DB
 }
 
-func (s *PostStore) Create(ctx context.Context) error {
+// Implementation Functions
+
+func (s *PostStore) Create(ctx context.Context, post *Post) error {
+	query := `
+	INSERT INTO posts (content, title, user_id, tags)
+	VALUES($1, $2, $3, $4) RETURNING id, created_at, updated_at
+	`
+
+	err := s.db.QueryRowContext(ctx, query, post.Content, post.Title, post.UserId, pq.Array(post.Tags)).Scan(
+		&post.ID, &post.CreatedAt, &post.UpdatedAt)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
--- internal/store/users.go
+++ internal/store/users.go
@@ -5,9 +5,18 @@ import (
 	"database/sql"
 )
 
+// Entity
+type User struct {
+	ID        int64  `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
+	CreatedAt string `json:"created_at"`
+}
+
 // UserRepository
 type UserRepository interface {
-	Create(context.Context) error
+	Create(context.Context, *User) error
 }
 
 // UserRepository Implementation
@@ -15,6 +24,26 @@ type UserStore struct {
 	db *sql.DB
 }
 
-func (s *UserStore) Create(ctx context.Context) error {
+// Implementation Functions
+
+func (s *UserStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO users (username, password, email) 
+		VALUES($1, $2, $3)
+    RETURNING id, created_at
+	`
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password,
+		user.Email,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
 	return nil
 }
