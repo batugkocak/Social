4555a2c68c70e4526fba7a5c63d22338c80fbdf8
Batug Kocak <batugkocak@gmail.com>
2025-01-10 01:18:14 +0300

    feat(api): add validation for create post payload fields

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -12,14 +12,19 @@ import (
 )
 
 type CreatePostPayload struct {
-	Content string   `json:"content"`
-	Title   string   `json:"title"`
+	Content string   `json:"content" validate:"required,max=100"`
+	Title   string   `json:"title" validate:"required,max=100"`
 	Tags    []string `json:"tags"`
 }
 
 func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
 	var payload CreatePostPayload
 	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
 		app.badRequestError(w, r, err)
 		return
 	}
