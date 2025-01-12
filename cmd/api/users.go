992bcb45ade0aedef5113be80d42409166a471ff
Batug Kocak <batugkocak@gmail.com>
2025-01-12 16:34:51 +0300

    feat(api): add user context middleware to handle getting users by their id TODO: follow and unfollow

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"net/http"
 	"strconv"
@@ -10,28 +11,56 @@ import (
 )
 
 func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
-	userID, paramErr := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
-	if paramErr != nil {
-		app.badRequestError(w, r, paramErr)
-		return
-	}
-
-	ctx := r.Context()
-	user, err := app.store.Users.GetById(ctx, userID)
-	if err != nil {
-		switch {
-		case errors.Is(err, store.ErrNotFound):
-			app.badRequestError(w, r, err)
-			return
-		default:
-			app.internalServerError(w, r, paramErr)
-			return
-		}
-	}
+	user := getUserFromCtx(r)
 
 	writeJsonErr := app.jsonResponse(w, http.StatusOK, user)
 	if writeJsonErr != nil {
 		app.internalServerError(w, r, writeJsonErr)
 		return
 	}
+}
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromCtx(r)
+}
+func (app *application) unfollowUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromCtx(r)
+}
+
+// Middlewares & Helpers
+
+type userKey string
+
+const userCtx userKey = "user"
+
+func (app *application) usersContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "userID")
+		id, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			app.internalServerError(w, r, err)
+			return
+		}
+
+		ctx := r.Context()
+
+		user, err := app.store.Users.GetById(ctx, id)
+		if err != nil {
+			switch {
+			case errors.Is(err, store.ErrNotFound):
+				app.notFoundError(w, r, err)
+			default:
+				app.internalServerError(w, r, err)
+			}
+			return
+		}
+
+		ctx = context.WithValue(ctx, userCtx, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getUserFromCtx(r *http.Request) *store.User {
+	user, _ := r.Context().Value(userCtx).(*store.User)
+	return user
 }
