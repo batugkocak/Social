28ce16afcb6743a2e49a189fa64b7b7b8da20208
Batug Kocak <batugkocak@gmail.com>
2025-01-18 16:33:06 +0300

    feat(api): implement pagination for user feed retrieval with sorting and limit options

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,18 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/batugkocak/social/internal/store"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
-	//TODO: Pagination, filters and sorting etc.
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
 
 	// Hard coded id until auth
-	feed, feedErr := app.store.Posts.GetUserFeed(r.Context(), int64(42))
+	feed, feedErr := app.store.Posts.GetUserFeed(r.Context(), int64(42), fq)
 	if feedErr != nil {
 		app.internalServerError(w, r, feedErr)
+		return
 	}
 
 	responseErr := app.jsonResponse(w, http.StatusOK, feed)
 	if responseErr != nil {
 		app.internalServerError(w, r, responseErr)
+		return
 	}
 }
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -35,7 +35,7 @@ type PostRepository interface {
 	GetById(context.Context, int64) (*Post, error)
 	DeleteById(context.Context, int64) error
 	UpdateById(context.Context, *Post) error
-	GetUserFeed(context.Context, int64) ([]*PostWithMetadata, error)
+	GetUserFeed(context.Context, int64, PaginatedFeedQuery) ([]*PostWithMetadata, error)
 }
 
 // PostRepository Implementation
@@ -150,7 +150,7 @@ func (s *PostStore) UpdateById(ctx context.Context, post *Post) error {
 
 }
 
-func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]*PostWithMetadata, error) {
+func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]*PostWithMetadata, error) {
 	query := `
 		SELECT 
 			p.id, p.user_id, p.title, p.content, p.created_at, p.version, p.tags,
@@ -162,11 +162,11 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]*PostWithM
 		JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
 		WHERE f.user_id = $1 OR p.user_id = $1
 		GROUP BY p.id, u.username
-		ORDER BY p.created_at DESC
-	`
+		ORDER BY p.created_at ` + fq.Sort +
+		` LIMIT $2 OFFSET $3`
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeDuration)
 	defer cancel()
-	rows, err := s.db.QueryContext(ctx, query, userID)
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
 	if err != nil {
 		return nil, err
 	}
