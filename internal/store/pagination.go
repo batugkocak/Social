1ed81c61bff78e04a11dc0d5f9dd0da93120308d
Batug Kocak <batugkocak@gmail.com>
2025-01-18 20:02:11 +0300

    feat(api): enhance pagination with search, tags, and date filters for user feeds

--- internal/store/pagination.go
+++ internal/store/pagination.go
@@ -3,12 +3,18 @@ package store
 import (
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type PaginatedFeedQuery struct {
-	Limit  int    `json:"limit" validate:"gte=1,lte=20"`
-	Offset int    `json:"offset" validate:"gte=0"`
-	Sort   string `json:"sort" validate:"oneof=asc desc"`
+	Limit  int      `json:"limit" validate:"gte=1,lte=20"`
+	Offset int      `json:"offset" validate:"gte=0"`
+	Sort   string   `json:"sort" validate:"oneof=asc desc"`
+	Tags   []string `json:"tags" validate:"max=5"`
+	Search string   `json:"search" validate:"max=100"`
+	Since  string   `json:"since" validate:"max=100"`
+	Until  string   `json:"until" validate:"max=100"`
 }
 
 func (fq PaginatedFeedQuery) Parse(r *http.Request) (PaginatedFeedQuery, error) {
@@ -37,6 +43,34 @@ func (fq PaginatedFeedQuery) Parse(r *http.Request) (PaginatedFeedQuery, error) 
 		fq.Sort = sort
 	}
 
-	return fq, nil
+	tags := qs.Get("tags")
+	if tags != "" {
+		fq.Tags = strings.Split(tags, ",")
+	}
 
+	search := qs.Get("search")
+	if search != "" {
+		fq.Search = search
+	}
+
+	since := qs.Get("since")
+	if since != "" {
+		fq.Since = parseTime(since)
+	}
+
+	until := qs.Get("until")
+	if until != "" {
+		fq.Since = parseTime(until)
+	}
+
+	return fq, nil
+}
+
+// Not sure if this is the right approach
+func parseTime(s string) string {
+	t, err := time.Parse(time.DateTime, s)
+	if err != nil {
+		return ""
+	}
+	return t.Format(time.DateTime)
 }
